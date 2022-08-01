c9556679fce28bba38dc9a11f55ca401b47a722e
Roman Nikiforov <rxafan@gmail.com>
2022-08-01 23:31:44 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,85 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/cheggaaa/pb/v3"
 )
+
+const readOnlyPermission = 0o444
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrOffsetNegativeValue   = errors.New("invalid negative offset value")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	fromPath, err := filepath.Abs(fromPath)
+	if err != nil {
+		return fmt.Errorf("can't get abs path: %v, err: %w", from, err)
+	}
+
+	fileFrom, err := os.OpenFile(fromPath, os.O_RDONLY, readOnlyPermission)
+	if err != nil {
+		return fmt.Errorf("open fromPath err: %w", err)
+	}
+	defer closeFile(fileFrom)
+
+	fileFromInfo, err := fileFrom.Stat()
+	if err != nil {
+		return fmt.Errorf("can't get file info, path: %v err: %w", from, err)
+	}
+
+	if !fileFromInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	if offset < 0 {
+		return ErrOffsetNegativeValue
+	}
+
+	if offset > fileFromInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	_, err = fileFrom.Seek(offset, 0)
+	if err != nil {
+		return fmt.Errorf("can't get offset from file: %v err: %w", from, err)
+	}
+
+	toPath, err = filepath.Abs(toPath)
+	if err != nil {
+		return fmt.Errorf("can't get abs path: %v, err: %w", toPath, err)
+	}
+	fileTo, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("can't create or open to file: %v err: %w", to, err)
+	}
+	defer closeFile(fileTo)
+
+	if limit == 0 || fileFromInfo.Size()-offset < limit {
+		limit = fileFromInfo.Size() - offset
+	}
+
+	reader := io.LimitReader(fileFrom, limit)
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(reader)
+	defer bar.Finish()
+
+	_, err = io.Copy(fileTo, barReader)
+	if err != nil {
+		return fmt.Errorf("can't copy: %w", err)
+	}
 	return nil
+}
+
+func closeFile(f *os.File) {
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,137 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
 
-func TestCopy(t *testing.T) {
-	// Place your code here.
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCopy_PositiveCases(t *testing.T) {
+	for _, tc := range []struct {
+		name   string
+		from   string
+		offset int64
+		limit  int64
+		result string
+	}{
+		{
+			name:   "offset & limit = 0; expect full copy",
+			from:   "testdata/input.txt",
+			offset: 0,
+			limit:  0,
+			result: "testdata/out_offset0_limit0.txt",
+		},
+		{
+			name:   "offset = 0, limit = 10; expect copy 10 bytes",
+			from:   "testdata/input.txt",
+			offset: 0,
+			limit:  10,
+			result: "testdata/out_offset0_limit10.txt",
+		},
+		{
+			name:   "offset = 0, limit = 1000; expect copy 1000 bytes",
+			from:   "testdata/input.txt",
+			offset: 0,
+			limit:  1000,
+			result: "testdata/out_offset0_limit1000.txt",
+		},
+		{
+			name:   "offset = 0, limit = 10000 > file from size; expect copy full file",
+			from:   "testdata/input.txt",
+			offset: 0,
+			limit:  10000,
+			result: "testdata/out_offset0_limit10000.txt",
+		},
+		{
+			name:   "offset = 100, limit = 1000; expect copy 1000 bytes",
+			from:   "testdata/input.txt",
+			offset: 100,
+			limit:  1000,
+			result: "testdata/out_offset100_limit1000.txt",
+		},
+		{
+			name:   "offset = 6000, limit = 1000; expect copy fileSize - offset bytes",
+			from:   "testdata/input.txt",
+			offset: 6000,
+			limit:  1000,
+			result: "testdata/out_offset6000_limit1000.txt",
+		},
+		{
+			name:   "offset = 0, limit = 6617; expect copy full file",
+			from:   "testdata/input.txt",
+			offset: 0,
+			limit:  6617,
+			result: "testdata/out_offset0_limit0.txt",
+		},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			tempFile, err := filepath.Abs("testdata")
+			require.NoError(t, err)
+			fileTo, err := os.CreateTemp(tempFile, "test_case")
+			require.NoError(t, err)
+			expResFile, err := os.Open(tc.result)
+			require.NoError(t, err)
+			defer func() {
+				_ = fileTo.Close()
+				_ = expResFile.Close()
+				_ = os.Remove(fileTo.Name())
+			}()
+			expRes, err := ioutil.ReadAll(expResFile)
+			require.NoError(t, err)
+
+			err = Copy(tc.from, fileTo.Name(), tc.offset, tc.limit)
+			require.NoError(t, err)
+
+			res, err := ioutil.ReadAll(fileTo)
+			require.NoError(t, err)
+			assert.Equal(t, expRes, res)
+		})
+	}
+}
+
+func TestCopy_NegativeCases(t *testing.T) {
+	for _, tc := range []struct {
+		name     string
+		from     string
+		offset   int64
+		limit    int64
+		expError error
+	}{
+		{
+			name:     "file with no size",
+			from:     "/dev/urandom",
+			offset:   0,
+			limit:    0,
+			expError: ErrUnsupportedFile,
+		},
+		{
+			name:     "negative offset",
+			from:     "testdata/input.txt",
+			offset:   -1,
+			limit:    0,
+			expError: ErrOffsetNegativeValue,
+		},
+		{
+			name:     "over offset",
+			from:     "testdata/input.txt",
+			offset:   6672,
+			limit:    0,
+			expError: ErrOffsetExceedsFileSize,
+		},
+	} {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			err := Copy(tc.from, "/tmp/", tc.offset, tc.limit)
+			assert.True(t, errors.Is(err, tc.expError))
+		})
+	}
 }
