59d7dfd84cb91b46e83844e82f05dc7503f49584
Roman Nikiforov <rxafan@gmail.com>
2022-08-30 16:54:28 +0300

    HW11 is finished

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,97 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/spf13/pflag"
+)
+
+var reg *regexp.Regexp
+
+type telnetFunc = func() error
+
+func init() {
+	reg = regexp.MustCompile(hostRegexp)
+}
+
+const (
+	maxPortValue   = 65535
+	minPortValue   = 1
+	defaultTimeout = 10 * time.Second
+	hostRegexp     = `\blocalhost\b|\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b`
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	address, timeout, err := parseParams()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	telnet := NewTelnetClient(address, *timeout, os.Stdin, os.Stdout)
+	if err = telnet.Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err := telnet.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	ctx, cancelFunc := context.WithCancel(context.Background())
+
+	go run(cancelFunc, telnet.Receive)
+	go run(cancelFunc, telnet.Send)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	select {
+	case <-sigCh:
+	case <-ctx.Done():
+		close(sigCh)
+	}
+}
+
+func run(cancelFunc context.CancelFunc, tlFunc telnetFunc) {
+	defer cancelFunc()
+
+	err := tlFunc()
+	if err != nil {
+		_, err = fmt.Fprintln(os.Stderr, err)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func parseParams() (address string, timeout *time.Duration, err error) {
+	timeout = pflag.Duration("timeout", defaultTimeout, "specify connection timeout, default: 10s")
+	pflag.Parse()
+
+	args := pflag.CommandLine.Args()
+	if len(args) != 2 {
+		return "", nil, fmt.Errorf("you must pass host & port")
+	}
+
+	host := args[0]
+	port := args[1]
+
+	if ok := reg.MatchString(host); !ok {
+		return "", nil, fmt.Errorf("invalid host")
+	}
+
+	if i, err := strconv.Atoi(port); err != nil || i < minPortValue || i > maxPortValue {
+		return "", nil, fmt.Errorf("invalid port")
+	}
+	return net.JoinHostPort(host, port), timeout, nil
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log"
+	"net"
 	"time"
 )
 
@@ -12,10 +15,75 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type Client struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &Client{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (c *Client) Connect() error {
+	var err error
+	c.conn, err = net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return fmt.Errorf("connect err: %w", err)
+	}
+	log.Printf("...Connected to %s", c.address)
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *Client) Close() error {
+	if c.conn == nil {
+		return fmt.Errorf("connection is nil")
+	}
+	if c.in == nil {
+		return fmt.Errorf("telnet in is nil")
+	}
+
+	if err := c.in.Close(); err != nil {
+		return fmt.Errorf("in io.ReadCloser close err: %w", err)
+	}
+
+	if err := c.conn.Close(); err != nil {
+		return fmt.Errorf("connection close err: %w", err)
+	}
+	return nil
+}
+
+func (c *Client) Send() error {
+	if c.conn == nil {
+		return fmt.Errorf("connection is nil")
+	}
+	if c.in == nil {
+		return fmt.Errorf("telnet in is nil")
+	}
+
+	if _, err := io.Copy(c.conn, c.in); err != nil {
+		return fmt.Errorf("send err: %w", err)
+	}
+	return nil
+}
+
+func (c *Client) Receive() error {
+	if c.conn == nil {
+		return fmt.Errorf("connection is nil")
+	}
+	if c.out == nil {
+		return fmt.Errorf("telnet out is nil")
+	}
+
+	if _, err := io.Copy(c.out, c.conn); err != nil {
+		return fmt.Errorf("receive err: %w", err)
+	}
+	return nil
+}
